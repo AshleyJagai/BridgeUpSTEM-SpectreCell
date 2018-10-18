188e574dae33b6b1c544f124ebd2f2359e236ee0
Ashley <Ashley.jagai.21@wheels-student.org>
2018-10-18 18:27:10 -0400

    fixed sixth code snippetst

--- units/unit_1/d04/code/code_snippet1.py
+++ units/unit_1/d04/code/code_snippet1.py
@@ -30,9 +30,25 @@ print(final_time_int)
 #Change current_time_str = input("What is the "current time" (in hours 0-23)?") to current_time_str = input("What is the 'current time' (in hours 0-23)?")
 
 str_time = input("What time is it now?")
-str_wait_time = input("What is the number of nours to wait?")
+str_wait_time = input("What is the number of hours to wait?")
 time = int(str_time)
-wai_time = int(str_wait_time)
+wait_time = int(str_wait_time)
 
 time_when_alarm_go_off = time + wait_time
-print(time_when_alarm_go_off)
+print (time_when_alarm_go_off)
+# Change wai_time = int(str_wait_time) to wait_time = int(str_wait_time)
+# Change str_wait_time = input("What is the number of nours to wait?") to str_wait_time = input("What is the number of hours to wait?")
+
+n = input("What time is it now (in hours)?")
+n = int(n)
+m = input("How many hours do you want to wait?")
+m = int(m)
+q = m % 12
+print("The time is now", q)
+# Change n = imt(n) to n = int(n)
+
+present_time = input("Enter the present timein hours:")
+set_alarm = input("Set the hours for alarm:")
+int (present_time, set_alarm)
+alarm_time = present_time + set_alarm
+print(alarm_time)
