5f4b66b8c8b033d04737fc6aa3dc057a3b1fcc98
colleencleary <cleary.ce@gmail.com>
2018-11-28 16:01:24 -0500

    added folder for helen fellow code

---
+++ colleen_code/project_processCSV.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from astrodbkit import astrodb
+
+# import gaia csv
+gaia_catalogue = pd.read_csv('gaia_data/all_catalog.csv')
+
+# import bdnyc database
+db = astrodb.Database('BDNYCdb_practice/bdnycdev_copy.db')
+
+db.query('select * from sources')
