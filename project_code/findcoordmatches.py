9ac50e2b824dec96adc0ec609b1222516ef0b266
Colleen Cleary <19698705+colleencleary@users.noreply.github.com>
2019-01-03 17:26:52 -0500

    Update findcoordmatches.py

--- project_code/findcoordmatches.py
+++ project_code/findcoordmatches.py
@@ -17,6 +17,11 @@ gaia_catalogue = pd.read_csv('gaia_data/all_catalog.csv')
 db = astrodb.Database('BDNYCdb_practice/bdnycdev_copy.db')
 
 
+# load matches and new_objects csvs
+# insert code here
+#
+#
+#
 
 
 # ===============================================
@@ -28,19 +33,14 @@ db_sources = db.query('SELECT * FROM sources', fmt='pandas')
 
 
 
-
-
 # ===============================================
 # create new empty dataframes to store gaia data
 # ===============================================
 
 # matches will store gaia data for objects in BDNYC database
-matches = pd.DataFrame(columns = gaia_catalogue.columns.values)
-print (matches.columns)
+matches = pd.DataFrame(columns=gaia_catalogue.columns.values)
 # new_objects will store gaia data for objects that do not exist in BDNYC database
-new_objects = pd.DataFrame(columns = gaia_catalogue.columns.values)
-print (new_objects.columns)
-
+new_objects = pd.DataFrame(columns=gaia_catalogue.columns.values)
 
 
 # ===============================================
@@ -48,24 +48,54 @@ print (new_objects.columns)
 # ===============================================
 
 for i in range(len(gaia_catalogue)):
-    #db.search((338.673, 40.694), "sources", radius=5)
-    #print (gaia_catalogue["RA"][i],gaia_catalogue["DEC"][i])
-    if (len(db.search((gaia_catalogue["RA"][i],gaia_catalogue["DEC"][i]),"sources",radius = 0.00084, fetch = True))) > 0:
-        #print (len(db.search((gaia_catalogue["RA"][i],gaia_catalogue["DEC"][i]),"sources",radius = 0.00084, fetch = True)))
+    if len(db.search((gaia_catalogue['RA'][i], gaia_catalogue['DEC'][i]), 'sources', radius=0.00084, fetch=True)) > 0:
         matches = matches.append(gaia_catalogue.loc[[i]])
+        # print(len(db.search((gaia_catalogue['RA'][i], gaia_catalogue['DEC'][i]), 'sources', radius=0.00084, fetch=True)))
     else:
         new_objects = new_objects.append(gaia_catalogue.loc[[i]])
-print (matches.head)
-len(matches)
+
 
 # ===============================================
 # Workspace
 # ===============================================
-
 # length of db_sources
-len(db_sources)
+# len(db_sources)
 
 # prints single row of gaia_catalogue
-gaia_catalogue.loc[[0]]
+# gaia_catalogue.loc[[0]]
 # prints first 5 rows of gaia_catalogue
-gaia_catalogue.head()
+# gaia_catalogue.head()
+
+# ===============================================
+# Plotting coordinates
+# ===============================================
+
+import astropy.coordinates as coord
+import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+
+# converting BDNYC database coordinates for plot
+db_ra = coord.Angle(pd.to_numeric(db_sources['ra']).fillna(np.nan).values*u.degree)
+db_ra = db_ra.wrap_at(180*u.degree)
+db_dec = coord.Angle(pd.to_numeric(db_sources['dec']).fillna(np.nan).values*u.degree)
+
+# converting matches csv coordinates
+matches_ra = coord.Angle(matches['RA'].values*u.degree)
+matches_ra = matches_ra.wrap_at(180*u.degree)
+matches_dec = coord.Angle(matches['DEC'].values*u.degree)
+
+# converting new_objects csv coordinates
+new_objects_ra = coord.Angle(new_objects['RA'].values*u.degree)
+new_objects_ra = new_objects_ra.wrap_at(180*u.degree)
+new_objects_dec = coord.Angle(new_objects['DEC'].values*u.degree)
+
+
+fig = plt.figure(figsize=(14,12))
+ax = fig.add_subplot(111, projection="mollweide")
+ax.set_facecolor('#17303F')
+plt.grid(True)
+ax.scatter(db_ra.radian, db_dec.radian, color="#E5E5E5", alpha=.8, edgecolors='face', label='in BDNYC database')
+ax.scatter(matches_ra.radian, matches_dec.radian, color="#F24333", label='in BDNYC database and GAIA dataset')
+ax.scatter(new_objects_ra.radian, new_objects_dec.radian, color="#E3B505", label='in GAIA dataset')
+ax.legend(loc=4)
